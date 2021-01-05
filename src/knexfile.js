71f8a27eadd86e5dfb4d4fce5a3cd5d52df24614
luldelrey <rpgstrikered@gmail.com>
2021-01-05 14:17:35 -0300

    problem with connection solved, creating migration of the link table

--- src/knexfile.js
+++ src/knexfile.js
@@ -6,44 +6,11 @@ module.exports = {
   development: {
     client: 'mysql',
     connection: {
-      host: '127.0.0.1',
+      host: 'localhost',
       user: OUSER,
       password: PASSWORD,
       database: DB_NAME,
     },
     debug: true,
   },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
-
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  }
-
 };
--- src/migrations/20210104110513_authors.js
+++ src/migrations/20210104110513_authors.js
@@ -3,6 +3,8 @@ exports.up = async function(knex) {
   return knex.schema.createTable('authors', (table) => {
     table.increments('id').primary();
     table.string('name', 50).notNullable();
+    table.string('email', 50).notNullable();
+    table.string('password', 50).notNullable();
     table.string('picture');
   });
 };
